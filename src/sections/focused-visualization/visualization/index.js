cb6c8648aac4505d9483c51a9f7a0f822527ead4
Matt Blanco <mattmblanco@gmail.com>
2024-03-23 20:52:21 -0400

    Change main component for each ring to reflect a year

--- src/sections/focused-visualization/visualization/index.js
+++ src/sections/focused-visualization/visualization/index.js
@@ -1,68 +1,57 @@
 import { useState } from "react"
 import { DashedCircle } from "../../../components/dashed-circle"
-import { Node } from "./node"
 import { ArtifactSummary } from "./artifactSummary"
+import { Node } from "./node"
 
 
 export function Visualization(props) {
   const width = window.innerWidth * 0.6
-  const height = window.innerHeight * 0.75
-  const initialRadius = (width * 0.05) / 2
-  const rings = Array(12).fill(0).map((v, i) => initialRadius + (i * initialRadius))
+  const height = window.innerHeight
+  const margin = width * 0.05
+  const initialRadius = ((width - margin * 2) * 0.06)
+  const rings = Object.keys(props.data)
+  const largestRadius = initialRadius * rings.length - 1
   const artifactSize = 6
   const [hoverArtifact, updateHover] = useState()
+  const boundaries = largestRadius + margin
 
   return (
-    <>
-      <svg width={width * 0.75} height={height}>
+    <div id="focusedVisualization">
+      <svg width={largestRadius * 2 + margin * 2} height={height}>
         {
-          rings.map((v, i, arr) => {
-            const largestRadius = arr[arr.length - 1]
-            const artifactsPerMonth = props.data.filter(artifact => artifact.month === i + 1)
+          rings.map((year, i) => {
+            const radius = ((i + 1) * initialRadius)
+            const textY = (((rings.length - i) * initialRadius) - 5) - margin / 2
 
             return (
               <g
                 id="focused"
                 key={`ring-${i}`}
               >
+                <DashedCircle
+                  key={`focues-ring-${i}`}
+                  fill={"none"}
+                  radius={radius}
+                  text={year}
+                  textX={boundaries}
+                  textY={textY}
+                  width={width}
+                  x={boundaries}
+                  y={boundaries - margin / 2}
+                  opacity={0.5}
+                />
                 {
-                  i === 0 ?
-                    <DashedCircle
-                      key={`focues-ring-${i}`}
-                      fill={"none"}
-                      radius={v}
-                      text={props.year}
-                      textX={width / 3}
-                      textY={largestRadius + 15}
-                      width={width}
-                      x={width / 3}
-                      y={largestRadius + 10}
-                    />
-                    :
-                    <DashedCircle
-                      key={`focues-ring-${i}`}
-                      fill={"none"}
-                      radius={v}
-                      text={props.year}
-                      width={width}
-                      x={width / 3}
-                      y={largestRadius + 10}
-                    />
-                }
-                {
-                  artifactsPerMonth.map((item, i, arr) => {
+                  props.data[year].sort((a, b) => a.month - b.month).map((artifact, i, arr) => {
                     return (
                       <Node
-                        key={`item-${props.year}-${i}`}
-                        index={i}
-                        item={item}
-                        value={v}
-                        arr={arr}
-                        width={width}
-                        artifactSize={artifactSize}
-                        year={props.year}
-                        largestRadius={largestRadius}
+                        r={artifactSize}
                         updateHover={updateHover}
+                        cx={boundaries}
+                        cy={boundaries - (margin / 2)}
+                        ringRadius={radius}
+                        artifact={artifact}
+                        arr={arr}
+                        index={i}
                       />
                     )
                   })
@@ -82,6 +71,6 @@ export function Visualization(props) {
             />
         }
       </div>
-    </>
+    </div>
   )
 }
